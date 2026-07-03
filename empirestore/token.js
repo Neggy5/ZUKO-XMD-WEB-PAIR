684061858161447721904a92348586ab12dabcfe
ZUKO-MD <132104247+Neggy5@users.noreply.github.com>
2026-07-04 00:32:39 +0100

    Add files via upload

---
+++ empirestore/token.js
@@ -0,0 +1,6 @@
+require('dotenv').config();
+
+module.exports = {
+  BOT_TOKEN: process.env.BOT_TOKEN || '8942092477:AAGPhfVoROjepdc4MDjawyXDOKl55b24ivY',
+  startupPassword: process.env.STARTUP_PASSWORD || 'empire'
+};
